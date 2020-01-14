c663d119a8e585a90b8721b072b5f67e1aed44b6
alokas-lm <529587581@qq.com>
2020-01-14 16:25:40 +0800

    第二次提交，已完成登录和验证

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import global from './assets/css/global.css'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+
+Vue.use(ElementUi)
+Vue.prototype.$axios = axios
+
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+
+// axios({
+//   method: 'post',
+//   url: '/login',
+//   data: {
+//     username: 'admin',
+//     password: '123456'
+//   }
+// }).then(res => {
+//   console.log(res);
+// });
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  global,
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const login = () => import('../components/Login')
+const home = () => import('../components/Home')
+
+
+Vue.use(Router)
+
+// noinspection JSAnnotator
+const routes = [
+    {
+      path: '',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: login
+    },
+    {
+      path: '/home',
+      component: home
+    },
+    ]
+
+const router = new Router({
+  routes,
+  mode: 'history',
+})
+
+router.beforeEach((to ,from, next) => {
+  //to 将要访问的路径
+  //from 代表从哪个路径跳转而来
+  //next 是一个函数，表示放行
+  //next() 放行   next('/login') 强制跳转
+  if (to.path == '/login') return next();
+  const tokenStr = window.sessionStorage.getItem('token')
+  if(!tokenStr) return next('/login')
+  next()
+})
+
+
+export default router
+
+
