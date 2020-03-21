7e47afb287a633ebfc5d9dc65ea0d30d5ebe5738
alokas-lm <529587581@qq.com>
2020-03-21 16:53:44 +0800

    第九次提交，完成数据报表部分

--- src/components/reports/Reports.vue
+++ src/components/reports/Reports.vue
@@ -1,23 +1,75 @@
 <template>
-  <h3>
-    报表组件
-  </h3>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <!--<div id="main" style="width: 750px;height:400px;"></div>-->
+    </el-card>
+  </div>
 </template>
 
 <script>
+  import echarts from 'echarts'
+  import _ from 'lodash'
+
   export default {
     name: "reports",
     data() {
       return {
-
+        echarts,
+        options: {
+          title: {
+            text: '用户来源'
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              label: {
+                backgroundColor: '#E9EEF3'
+              }
+            }
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [
+            {
+              boundaryGap: false
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value'
+            }
+          ]
+        }
       }
     },
-    created: {
+    created() {
 
     },
-    methods: {
+    async mounted() {
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(document.getElementById('main'));
 
-    }
+      const {data: res} = await this.$axios.get('reports/type/1')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取折线图数据失败')
+      }
+      console.log(res)
+      //数据处理
+      const opt = _.merge(res.data, this.options)
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(opt);
+    },
+    methods: {}
   }
 </script>
 
